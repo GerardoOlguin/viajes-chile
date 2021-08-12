524992aad64a9f79835e48c2cda3cac5e977753f
GerardoOlguin <88815207+GerardoOlguin@users.noreply.github.com>
2021-08-12 00:31:55 -0400

    Add files via upload

---
+++ Viajes chile/assets/js/scrip.js
@@ -0,0 +1,30 @@
+
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      });
+      
+     
+     
+     
+     
+     
+     $( "#p-1" ).click(function() {
+        $( "#p1" ).toggle();
+      });
+
+
+
+    $( "#p-2" ).click(function() {
+        $( "#p2" ).toggle();
+      });
+      
+      $( "#p-3" ).click(function() {
+        $( "#p3" ).toggle();
+      });
+      
+      $( "#p-4" ).click(function() {
+        $( "#p4" ).toggle();
+      });
+   
+  
+  
